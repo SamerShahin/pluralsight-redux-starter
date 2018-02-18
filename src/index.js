318cb68e0311dfbe0ab913a9e44e98136fbd0c46
samershahin <samer.shahin@galilsoftware.com>
2018-02-18 18:26:18 +0200

    create store , courses dispatcher/actions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import 'babel-polyfill'; //babel transpile es6 to es5 , but there is a set of features in es6 that babel cannot transpile and for those we use the pollyfill
 import React from 'react';
 import { render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import './styles/styles.css'; //Webpack can import css files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+//pass the initial state here
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes}/>,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>,
   document.getElementById("app")
 );
---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,11 @@
+export default function courseReducer(state = [],action) {
+  switch (action.type){
+    case "CREATE_COURSE":
+      //use spread  and create a new copy of the course because redux state is immutable and thus we need to create a new object and not change the current one
+      return [...state,
+        Object.assign({},action.course)
+      ];
+    default:
+      return state;
+  }
+}
