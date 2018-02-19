d780581ba2e2c2d1ba2a6073e55a9d3c5313347d
samershahin <samer.shahin@galilsoftware.com>
2018-02-19 12:32:47 +0200

    handling async calls in redux

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,20 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+
 export function createCourse(course) {
   return { type: types.CREATE_COURSE , course: course};
+}
+
+export function loadCoursesSuccess(courses) {
+  return {type: types.LOAD_COURSES_SUCCESS , courses: courses};
+}
+//every thunk always return a function that accept a dispatch
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }
--- src/componenets/course/CoursesPage.js
+++ src/componenets/course/CoursesPage.js
@@ -2,26 +2,11 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.state = {
-      course: {title: ""}
-    };
-    //binding the "this" to the functions in the constructor
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave(event) {
-    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -29,20 +14,11 @@ class CoursesPage extends React.Component {
   }
 
   render() {
+    const {courses} = this.props;
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}/>
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave}/>
-
+        <CourseList courses={courses}/>
       </div>
     );
   }
